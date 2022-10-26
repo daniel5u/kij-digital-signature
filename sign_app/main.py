26395e9f0e89c10ab989c2617a4a2391bf32be04
Daniel Sugianto <111328711+daniel5u@users.noreply.github.com>
2022-10-26 22:14:57 +0700

    feat(sign_app): add hash file using sha256

--- sign_app/main.py
+++ sign_app/main.py
@@ -1,6 +1,10 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import Enum
+import hashlib
+import os
+
+BUF_SIZE = 65536
 
 
 class CryptoHashFunctionOption(Enum):
@@ -17,18 +21,38 @@ class ICryptoHashFunction(ABC):
         raise NotImplementedError
 
 
+class SHA256(ICryptoHashFunction):
+    def __init__(self, bufferSize):
+        self.bufferSize = bufferSize
+
+    # https://stackoverflow.com/questions/22058048/hashing-a-file-in-python
+    def hashFile(self, path):
+        sha256 = hashlib.sha256()
+
+        with open(path, 'rb') as f:
+            while True:
+                data = f.read(self.bufferSize)
+                if not data:
+                    break
+                sha256.update(data)
+
+        return sha256.hexdigest()
+
+
 class KeyPair:
-    pass
-
-
-class MainController:
     pass
 
 
 @dataclass
 class InputData:
     chfOpt: str
-    path: str
+    filePath: str
+
+
+@dataclass
+class OutputData:
+    chfOpt: str
+    hashValue: str
 
 
 class MainView:
@@ -43,19 +67,56 @@ class MainView:
 
     def getInput(self) -> InputData:
         chfOpt = input("Algorithm: ")
-        path = input("File path: ")
-        return InputData(chfOpt, path)
+        filePath = input("File path: ")
+        return InputData(chfOpt, filePath)
+
+    def printOutput(self, outputData: OutputData):
+        print(f"Algorithm: {CryptoHashFunctionOption(outputData.chfOpt).name}")
+        print(f"Hash: {outputData.hashValue}")
 
 
 class SignApp:
     def __init__(self):
         self.view = MainView()
 
+    def validateInput(self, inputData: InputData):
+        if not inputData.chfOpt.isdigit():
+            raise Exception("Hash algorithm option must be a number")
+
+        if not os.path.isfile(inputData.filePath):
+            raise Exception("File not found")
+
     def run(self):
         while True:
             print("Hash algorithms:")
             self.view.printCryptoHashFunctionOptions()
-            input = self.view.getInput()
+            inputData = self.view.getInput()
+            isInputValid = True
+
+            try:
+                self.validateInput(inputData)
+            except Exception as e:
+                print(f"ERROR: {e}")
+                isInputValid = False
+
+            if not isInputValid:
+                continue
+
+            chfOpt = int(inputData.chfOpt)
+
+            hashValue = ""
+
+            if chfOpt == CryptoHashFunctionOption.SHA256.value:
+                sha256 = SHA256(BUF_SIZE)
+                hashValue = sha256.hashFile(inputData.filePath)
+
+            print("")
+
+            self.view.printOutput(OutputData(
+                chfOpt,
+                hashValue
+            ))
+
             print("")
 
 

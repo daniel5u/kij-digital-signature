8f71ad55ec3706d4b19e65715e5aa2062c19530b
Daniel Sugianto <111328711+daniel5u@users.noreply.github.com>
2022-10-26 21:34:11 +0700

    feat(sign_app): add get input

--- sign_app/main.py
+++ sign_app/main.py
@@ -1,7 +1,17 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from enum import Enum
 
 
-class IHashFunction(ABC):
+class CryptoHashFunctionOption(Enum):
+    SHA1 = 0
+    SHA224 = 1
+    SHA256 = 2
+    SHA384 = 3
+    SHA512 = 4
+
+
+class ICryptoHashFunction(ABC):
     @abstractmethod
     def hashFile(self, path) -> str:
         raise NotImplementedError
@@ -15,12 +25,43 @@ class MainController:
     pass
 
 
+@dataclass
+class InputData:
+    chfOpt: str
+    path: str
+
+
 class MainView:
-    pass
+    def __init__(self):
+        self.cryptoHashFunctionOptions = {
+            i.name: i.value for i in CryptoHashFunctionOption
+        }
+
+    def printCryptoHashFunctionOptions(self):
+        for key, val in self.cryptoHashFunctionOptions.items():
+            print(f"[{val}] {key}")
+
+    def getInput(self) -> InputData:
+        chfOpt = input("Algorithm: ")
+        path = input("File path: ")
+        return InputData(chfOpt, path)
+
+
+class SignApp:
+    def __init__(self):
+        self.view = MainView()
+
+    def run(self):
+        while True:
+            print("Hash algorithms:")
+            self.view.printCryptoHashFunctionOptions()
+            input = self.view.getInput()
+            print("")
 
 
 def main():
-    print("Hello World")
+    app = SignApp()
+    app.run()
 
 
 if __name__ == '__main__':

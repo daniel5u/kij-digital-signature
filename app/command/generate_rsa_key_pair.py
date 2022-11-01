e9042eb777e69a44c16dd5638c35f1212c676d38
WiliamH.W <wiliamwijaya1985@gmail.com>
2022-11-01 22:33:51 +0700

    refactor: remove ./

--- app/command/generate_rsa_key_pair.py
+++ app/command/generate_rsa_key_pair.py
@@ -7,21 +7,17 @@ class GenerateRSAKeyPair:
     @staticmethod
     def do():
         privateKey = PyCryptodomeRSA.generateKeyPair(RSA_KEY_PAIR_BITS)
-        dstDirPath = os.path.join(
-                        "./",
-                        KEY_PAIR_DIR_NAME
-                    )
         PyCryptodomeRSA.exportKey(
             privateKey,
             os.path.join(
-                dstDirPath,
+                KEY_PAIR_DIR_NAME,
                 PRIVATE_KEY_FILE_NAME
             )
         )
         PyCryptodomeRSA.exportKey(
             privateKey.public_key(),
             os.path.join(
-                dstDirPath,
+                KEY_PAIR_DIR_NAME,
                 PUBLIC_KEY_FILE_NAME
             )
         )

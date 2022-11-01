440206562b8b95b692cf5523c6f76c5f7598f6f2
Putu Krisna Andyartha <putukrisna6@gmail.com>
2022-11-01 07:24:07 +0700

    feat: use logging library + consistency

--- app/command/verify.py
+++ app/command/verify.py
@@ -1,9 +1,9 @@
 from dto.verify_request import VerifyRequest
 from dto.verify_response import VerifyResponse
 from util.hash import getHash
+from util.file import printException
 from crypto.rsa import PyCryptodomeRSA
 from constant import KEY_PAIR_DIR_NAME, PUBLIC_KEY_FILE_NAME
-
 
 class Verify:
     @staticmethod
@@ -14,7 +14,7 @@ class Verify:
                 verifyRequest.filePath
             )
         except Exception as e:
-            print(f"ERROR: {e}")
+            printException(e)
             return
 
         publicKeyPath = f"{KEY_PAIR_DIR_NAME}/{PUBLIC_KEY_FILE_NAME}"
